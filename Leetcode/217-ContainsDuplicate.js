8b6197e33d7c7d0fa5dca42f142326fc945c33b5
Justin Solo <justinpaulosolo@gmail.com>
2022-10-20 04:31:05 -0700

    Solved Leetcode #217.

---
+++ Leetcode/217-ContainsDuplicate.js
@@ -0,0 +1,15 @@
+const nums = [1,2,3];
+var containsDuplicate = function(nums) {
+    let temp = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        temp = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === temp) {
+                return true
+            }
+        }
+    }
+    return false;
+};
+console.log(containsDuplicate(nums));
