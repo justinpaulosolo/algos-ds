358a53385dfea5942020a6b5b7157f3818b44bc3
Justin Solo <justinpaulosolo@gmail.com>
2022-10-20 23:32:31 -0700

    DepthFirstTraversal

--- Algos-DS/Trees/dft.js
+++ Algos-DS/Trees/dft.js
@@ -1,17 +1,17 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.right = null;
-        this.left = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.right = null;
+    this.left = null;
+  }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
 
 //            a
 //          /   \
@@ -27,61 +27,61 @@ c.right = f;
 
 // Iterative
 const depthFirstPrint = (root) => {
-    const stack = [ root ];
+  const stack = [root];
 
-    while(stack.length > 0) {
-        const curr = stack.pop();
-        console.log(curr.val);
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    console.log(curr.val);
 
-        if (curr.right !== null) {
-            stack.push(curr.right)
-        }
-        if (curr.left !== null) {
-            stack.push(curr.left)
-        }
+    if (curr.right !== null) {
+      stack.push(curr.right);
     }
+    if (curr.left !== null) {
+      stack.push(curr.left);
+    }
+  }
 }; // O(n) time, O(n) space
 
 // recursion
 const depthFirstPrintV2 = (root) => {
-    // the tree is empty
-    if (root === null) return;
+  // the tree is empty
+  if (root === null) return;
 
-    console.log(root.val);
-    depthFirstPrintV2(root.left);
-    depthFirstPrintV2(root.right);
+  console.log(root.val);
+  depthFirstPrintV2(root.left);
+  depthFirstPrintV2(root.right);
 }; // O(n) time, O(n) space
 
 // pre-order traversal
 // self, left, right
 const preOrder = (root) => {
-    if (root === null) return;
+  if (root === null) return;
 
-    console.log(root.val);
+  console.log(root.val);
 
-    preOrder(root.left);
-    preOrder(root.right);
+  preOrder(root.left);
+  preOrder(root.right);
 };
 
 // post-order traversal
 // left, right, sefl
 const postOrder = (root) => {
-    if (root === null) return;
+  if (root === null) return;
 
-    preOrder(root.left);
-    preOrder(root.right);
+  preOrder(root.left);
+  preOrder(root.right);
 
-    console.log(root.val);
+  console.log(root.val);
 };
 
 // post-order traversal
 // left, right, self
 const inOrder = (root) => {
-    if (root === null) return;
+  if (root === null) return;
 
-    preOrder(root.left);
-    console.log(root.val);
-    preOrder(root.right);
+  preOrder(root.left);
+  console.log(root.val);
+  preOrder(root.right);
 };
 
 const g = new Node(3);
@@ -104,8 +104,8 @@ j.right = m;
 //     4    -2      5
 
 const totalSum = (root) => {
-    if(root === null) return 0;
-    return totalSum(root.left) + root.val + totalSum(root.right);
+  if (root === null) return 0;
+  return totalSum(root.left) + root.val + totalSum(root.right);
 };
 
 //depthFirstPrint(a)
